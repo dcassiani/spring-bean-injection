015807627f5f0b8aa271dca9592d0969c5dc0611
Daniel Cassiani <dcassiani@gmail.com>
2017-02-16 13:06:48 -0200

    less sysouts

--- src/main/java/com/sql/Main.java
+++ src/main/java/com/sql/Main.java
@@ -8,13 +8,18 @@ import com.sql.database.DatabaseBean;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "beans.xml" });
+        ApplicationContext context = 
+            new ClassPathXmlApplicationContext(new String[] { "beans.xml" });
         DatabaseBean databaseBean = (DatabaseBean) context.getBean("databaseBean");
-        System.out.println("path: " + databaseBean.getDatabasePath());
-        System.out.println("port: " + databaseBean.getPort());
-        System.out.println("login: " + databaseBean.getLogin());
-        System.out.println("password: " + databaseBean.getPassword()
-            .getStr());
+        
+        String credentials = databaseBean.getLogin()
+            .concat(":")
+            .concat(databaseBean.getPassword().getStr())
+            .concat("@")
+            .concat(databaseBean.getDatabasePath())
+            .concat(":")
+            .concat(Integer.toString(databaseBean.getPort()));
+        System.out.println(credentials);
     }
 
 }
